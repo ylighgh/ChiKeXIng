ec85822d676e2fd452dd464cb30fdddee4f8a701
ylighgh <475254161@qq.com>
2022-05-11 16:49:26 +0800

    update UserModel

--- models.py
+++ models.py
@@ -2,11 +2,22 @@ from exts import db
 from datetime import datetime
 
 
-class User_setting(db.Model):
-    # 表名
-    __tablename__ = 'user_setting'
-    # 字段名
+class EmailCaptchaModel(db.Model):
+    __tablename__ = "email_captcha"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    user_id = db.Column(db.String(200), nullable=False)
-    user_email = db.Column(db.String(200), nullable=False)
-    user_password = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    captcha = db.Column(db.String(10), nullable=False)
+    create_time = db.Column(db.DateTime, default=datetime.now)
+
+
+class UserModel(db.Model):
+    __tablename__ = "user"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(200), nullable=False)
+    join_time = db.Column(db.DateTime, default=datetime.now)
+    phone = db.Column(db.String(100), nullable=True)
+    address = db.Column(db.String(100), nullable=True)
+    signature = db.Column(db.String(100), nullable=True)
+    introduction = db.Column(db.String(100), nullable=True)
